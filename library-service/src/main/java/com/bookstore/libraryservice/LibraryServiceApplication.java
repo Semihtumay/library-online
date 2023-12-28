0657ba0d775116c01d174c8e7a03a9292aa1166b
Semih Tümay Balcı <semih.tumayb@gmail.com>
2023-12-29 02:29:31 +0300

    added api-gateway

--- library-service/src/main/java/com/bookstore/libraryservice/LibraryServiceApplication.java
+++ library-service/src/main/java/com/bookstore/libraryservice/LibraryServiceApplication.java
@@ -1,8 +1,12 @@
 package com.bookstore.libraryservice;
 
+import com.bookstore.libraryservice.client.RetrieveMessageErrorDecoder;
+import feign.Logger;
+import feign.codec.ErrorDecoder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableFeignClients
@@ -11,5 +15,4 @@ public class LibraryServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(LibraryServiceApplication.class, args);
 	}
-
 }
--- library-service/src/main/java/com/bookstore/libraryservice/client/BookServiceClient.java
+++ library-service/src/main/java/com/bookstore/libraryservice/client/BookServiceClient.java
@@ -2,6 +2,9 @@ package com.bookstore.libraryservice.client;
 
 import com.bookstore.libraryservice.dto.BookDto;
 import com.bookstore.libraryservice.dto.BookIdDto;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,9 +13,27 @@ import org.springframework.web.bind.annotation.PathVariable;
 @FeignClient(name = "book-service", path = "/v1/book")
 public interface BookServiceClient {
 
+    Logger logger = LoggerFactory.getLogger(BookServiceClient.class);
+
     @GetMapping("/isbn/{isbn}")
-    ResponseEntity<BookIdDto> getBookByIsbn(@PathVariable String isbn);
+    @CircuitBreaker(name = "getBookByIsbnCircuitBreaker", fallbackMethod = "getBookFallback")
+    ResponseEntity<BookIdDto> getBookByIsbn(@PathVariable(value = "isbn") String isbn);
 
     @GetMapping("/{id}")
-    ResponseEntity<BookDto> getBookById(@PathVariable String id);
+    @CircuitBreaker(name = "getBookByIdCircuitBreaker", fallbackMethod = "getBookIdFallback")
+    ResponseEntity<BookDto> getBookById(@PathVariable(value = "id") String id);
+
+    default ResponseEntity<BookIdDto> getBookFallback(String isbn, Exception exception){
+        logger.info("Book not found by isbn: " + isbn + ", returning default BookIdDto object." );
+        return ResponseEntity.ok(new BookIdDto("default-bookId", "default-isbn"));
+    }
+
+    default ResponseEntity<BookDto> getBookIdFallback(String isbn, Exception exception){
+        logger.info("Book not found by isbn: " + isbn + ", returning default BookIdDto object." );
+        return ResponseEntity.ok(new BookDto(new BookIdDto("default-bookId", "default-isbn"),
+                "default-title",
+                0,
+                "default-author",
+                "default-press-name"));
+    }
 }
