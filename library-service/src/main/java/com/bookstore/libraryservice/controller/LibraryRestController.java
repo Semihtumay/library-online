3bf21b048cc9a2a083df20378c401b2c938fc767
Semih Tümay Balcı <semih.tumayb@gmail.com>
2024-01-05 01:52:27 +0300

    added vault

--- library-service/src/main/java/com/bookstore/libraryservice/controller/LibraryRestController.java
+++ library-service/src/main/java/com/bookstore/libraryservice/controller/LibraryRestController.java
@@ -5,6 +5,7 @@ import com.bookstore.libraryservice.dto.LibraryDto;
 import com.bookstore.libraryservice.service.LibraryService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,6 +14,9 @@ import org.springframework.web.bind.annotation.*;
 public class LibraryRestController {
     private final LibraryService libraryService;
     Logger logger = LoggerFactory.getLogger(LibraryRestController.class);
+
+    @Value("${library-service.book.count}")
+    private String count;
 
 
     public LibraryRestController(LibraryService libraryService) {
@@ -33,6 +37,11 @@ public class LibraryRestController {
     public ResponseEntity<Void> addBookToLibrary(@RequestBody AddBookRequest addBookRequest){
          libraryService.addBookToLibrary(addBookRequest);
          return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/count")
+    public ResponseEntity<String> getCount() {
+        return ResponseEntity.ok("Library count is" + count);
     }
 
 
